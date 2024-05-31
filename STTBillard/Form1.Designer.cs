7f3df5d5908fe892286ba8c5e280a2aa7eed0bd3
LocNguyen01 <152239488+LocNguyen01@users.noreply.github.com>
2024-05-31 19:20:48 +0700

    ad

--- STTBillard/Form1.Designer.cs
+++ STTBillard/Form1.Designer.cs
@@ -102,10 +102,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(8, 206);
+            this.label1.Font = new System.Drawing.Font("Arial", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(4, 206);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(164, 12);
+            this.label1.Size = new System.Drawing.Size(184, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "Quý khách vui lòng đợi đến số thứ tự này!";
             // 
